ed81ef52b012fc72b4dbeb5d416045b8fc4a157d
Mikael Öhman <micketeer@gmail.com>
2011-08-07 23:07:43 +0200

    Towards a general card stack.

--- data/CardStack.java
+++ data/CardStack.java
@@ -4,26 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A stack of one or more cards
- *  
+ * A stack of zero or more cards
+ *
  * @author mahogny
  *
  */
-public class CardStack
+public interface CardStack<E>
 	{
 
 	/**
 	 * We have to think of representing games like MTG - a recursive definition might be better
-	 * 
-	 * 
+	 *
+	 *
 	 */
-	public List<Card> cards=new ArrayList<Card>();
-	
-	
-	
-	public void addCard(Card c)
+	public List<E> cards=new ArrayList<E>();
+
+	public void addCard(E c)
 		{
 		cards.add(c);
 		}
-	
+
+    public E drawCard()
+        {
+        E card = cards.get(0);
+        cards.remove(0);
+        return card;
+        }
+
+    public CardStack<E> drawCards(int n)
+        {
+        CardStack<E> cards = new CardStack<E>();
+        List<E> cl = cards.subList(0,n);
+        cards.cards.addAll(cl);
+        cl.clear();
+        return cards;
+        }
+
+
 	}
