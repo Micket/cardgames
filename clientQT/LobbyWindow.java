603036271d12cb3af061db69c668e2db821fb3d3
Mikael Öhman <micketeer@gmail.com>
2011-08-12 22:24:52 +0200

    Tweaking the lobby.

--- clientQT/LobbyWindow.java
+++ clientQT/LobbyWindow.java
@@ -1,23 +1,32 @@
 package clientQT;
 
 
+import java.util.*;
+
 import action.Message;
 import action.UserAction;
 import action.UserActionLobbyMessage;
 
 import clientData.ServerListener;
 
+import com.trolltech.qt.core.Qt;
 import com.trolltech.qt.gui.QAction;
 import com.trolltech.qt.gui.QApplication;
-import com.trolltech.qt.gui.QDialog;
 import com.trolltech.qt.gui.QLineEdit;
 import com.trolltech.qt.gui.QMenuBar;
 import com.trolltech.qt.gui.QPushButton;
 import com.trolltech.qt.gui.QTextEdit;
+import com.trolltech.qt.gui.QSplitter;
 import com.trolltech.qt.gui.QVBoxLayout;
 import com.trolltech.qt.gui.QHBoxLayout;
 import com.trolltech.qt.gui.QInputDialog;
 import com.trolltech.qt.gui.QSizePolicy;
+import com.trolltech.qt.gui.QTableWidget;
+import com.trolltech.qt.gui.QTableWidgetItem;
+
+import com.trolltech.qt.gui.QTreeWidget;
+import com.trolltech.qt.gui.QTreeWidgetItem;
+
 import com.trolltech.qt.gui.QWidget;
 
 public class LobbyWindow extends QWidget implements ServerListener
@@ -27,6 +36,9 @@ public class LobbyWindow extends QWidget implements ServerListener
 	private QAction miExit;
 
 	private QPushButton bNick;
+	
+	private QTreeWidget userList;
+	private QTableWidget gameList;
 	
 	private QTextEdit chatHistory;
 	
@@ -38,7 +50,7 @@ public class LobbyWindow extends QWidget implements ServerListener
 	
 	public void actionExit()
 		{
-		System.out.println("hello");
+		System.out.println("Exiting.");
 		System.exit(0);
 		//TODO handle connections
 		}
@@ -77,11 +89,14 @@ public class LobbyWindow extends QWidget implements ServerListener
 		this.client=client;
 
 		QHBoxLayout lobbyLayout=new QHBoxLayout();
-		QVBoxLayout userAndGameLayout=new QVBoxLayout();
+		//QSplitter lobbyLayout=new QSplitter();
+		//lobbyLayout.setOrientation(Qt.Orientation.Horizontal);
 		QVBoxLayout chatWindowLayout=new QVBoxLayout();
 		QHBoxLayout chatInputLayout=new QHBoxLayout();
+		QSplitter userAndGameLayout = new QSplitter();
+		userAndGameLayout.setOrientation(Qt.Orientation.Vertical);
 
-		//layoutV.setMenuBar()
+
 		nick = "Mahogny";
 		bNick=new QPushButton(nick+":", this);
 		bNick.clicked.connect(this, "actionChangeNick()");
@@ -89,20 +104,48 @@ public class LobbyWindow extends QWidget implements ServerListener
 		chatHistory=new QTextEdit(this);
 		chatHistory.setReadOnly(true);
 		
-		
 		tfEditLine=new QLineEdit(this);
 		tfEditLine.returnPressed.connect(this,"actionSendMessage()");
 
+		userList=new QTreeWidget();
+		
+		//userList.setColumnCount(2);
+		//userList.setColumnWidth(0,30);
+
+		gameList=new QTableWidget();
+		gameList.setColumnCount(2);
+		gameList.setColumnWidth(0,30);
+		gameList.setShowGrid(false);
+		
+		// TODO: Real user list
+		List<String> testuserentry = new ArrayList<String>();
+		testuserentry.add(nick);
+		testuserentry.add("Come at me!");
+		QTreeWidgetItem testUser = new QTreeWidgetItem(testuserentry);
+		userList.insertTopLevelItem(0, testUser);
+		userList.setHeader(null);
+		//userList.setRowCount(1);
+		//userList.setItem(0,1,newUserItem);
+		// TODO: Real game list
+		QTableWidgetItem newGameItem=new QTableWidgetItem("BlackJack");
+		QTableWidgetItem newGameUsersItem=new QTableWidgetItem("1/6");
+		gameList.setRowCount(1);
+		gameList.setItem(0,0,newGameUsersItem);
+		gameList.setItem(0,1,newGameItem);
+
+		// Lobby layout
 		setLayout(lobbyLayout);
 		lobbyLayout.addLayout(chatWindowLayout);
-		lobbyLayout.addLayout(userAndGameLayout);
+		lobbyLayout.addWidget(userAndGameLayout);
 		chatWindowLayout.addWidget(chatHistory);
 		chatWindowLayout.addLayout(chatInputLayout);
 		chatInputLayout.addWidget(bNick);
 		chatInputLayout.addWidget(tfEditLine);
+		userAndGameLayout.addWidget(userList);
+		userAndGameLayout.addWidget(gameList);
 		
 		
-		setWindowTitle(tr("Lobby"));
+		setWindowTitle("Lobby");
 
 		/*
 		menuBar=new QMenuBar();
