29c57aefdac82e95247d637c6068fc0e42536590
Mikael Öhman <micketeer@gmail.com>
2011-08-14 16:50:48 +0200

    Some effort towards a list of available games in lobby.

--- action/UserActionStartGame.java
+++ action/UserActionStartGame.java
@@ -11,4 +11,5 @@ public class UserActionStartGame extends UserAction
 	private static final long serialVersionUID = 1L;
 	
 	public Class<? extends GameLogic> game;
+	public String customName; // User set name.
 	}
