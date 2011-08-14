3e2a3e92155a93064973adf14d48fbe3e7e74c9b
Johan Henriksson <mahogny@areta.org>
2011-08-14 19:35:35 +0200

    work on connect dialog

--- clientQT/ConnectToServerDialog.java
+++ clientQT/ConnectToServerDialog.java
@@ -9,25 +9,26 @@ import com.trolltech.qt.gui.QPushButton;
 
 public class ConnectToServerDialog extends QDialog
 	{
-	
+	QLabel labelAddress;
+	QLabel labelPort;
+  
 	public ConnectToServerDialog()
 		{
-		QLabel labelAddress = new QLabel(tr("Address:"), this);
-		QLabel labelPort = new QLabel(tr("Port:"), this);
+		labelAddress = new QLabel(tr("Address:"), this);
+		labelPort = new QLabel(tr("Port:"), this);
 		QLineEdit editAddress = new QLineEdit(this);
 		QLineEdit editPort = new QLineEdit(this);
-/*
-    QPushButton bConnect=new QPushButton(tr("Connect"), this);
-    bConnect.clicked.connect(this, "actionConnect()");
 
-    QPushButton bCancel=new QPushButton(tr("Cancel"), this);
+    QPushButton bConnect=new QPushButton(tr("Connect"));
+    QPushButton bCancel=new QPushButton(tr("Cancel"));
+    bConnect.clicked.connect(this, "actionConnect()");
     bCancel.clicked.connect(this, "actionCancel()");
     
     
-    QHBoxLayout buttonsLayout = new QHBoxLayout(this);
+    QHBoxLayout buttonsLayout = new QHBoxLayout();
     buttonsLayout.addStretch();
     buttonsLayout.addWidget(bConnect);
-    buttonsLayout.addWidget(bCancel);*/
+    buttonsLayout.addWidget(bCancel);
 
 		
 	   QGridLayout mainLayout = new QGridLayout();
@@ -35,12 +36,12 @@ public class ConnectToServerDialog extends QDialog
 	   mainLayout.addWidget(editAddress, 0, 1);
 	   mainLayout.addWidget(labelPort, 1, 0);
 	   mainLayout.addWidget(editPort, 1, 1);
-//	   mainLayout.addLayout(buttonsLayout, 2, 0);
+	   mainLayout.addLayout(buttonsLayout, 2, 0, 1,2);
 	   setLayout(mainLayout);
 	   
 
 	   setWindowTitle(tr("Connect to server"));
-//	   resize(700, 300);
+//	   resize(300, 100);
 
      
 }
@@ -48,6 +49,10 @@ public class ConnectToServerDialog extends QDialog
 	public void actionConnect()
 		{
 		System.out.println("Connect!!!");
+		
+		String addr=labelAddress.text();
+		String portS=labelPort.text();
+		
 		}
 	
 	public void actionCancel()
