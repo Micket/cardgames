65649d5c8b6bfbf08d3cc5845c9d8d918c32b6b3
Mikael Öhman <micketeer@gmail.com>
2011-08-16 23:51:48 +0200

    More card functionality

--- serverData/PlayingCard.java
+++ serverData/PlayingCard.java
@@ -7,18 +7,18 @@ import clientData.ClientCard;
  */
 public class PlayingCard extends ServerCard
 	{
-	
+
 	public boolean showsFront=false;
 	public int rotation=0; //Can be 0,1,2,3, going clockwise
-		
-	
+
+
     public enum Suit { Clubs, Diamonds, Hearts, Spades, RedJoker, BlackJoker } // Jokers can often be only red or black
     public enum Rank
         {
         Deuce(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8), Nine(9), Ten(10), Jack(11), Queen(12), King(13), Ace(1), Joker(0); // Include the joker here?
         private int value;
         private Rank(int n) { value = n; }
-        public int giveValue() { return value; }
+        public int getValue() { return value; }
         }
 
     private Suit suit;
@@ -32,19 +32,20 @@ public class PlayingCard extends ServerCard
 
     /// Convenience checks;
     public boolean isAce() { return rank == Rank.Ace; }
+		public boolean isFace() { return rank.getValue() > 10; }
     public boolean isJoker() { return rank == Rank.Joker; }
     public boolean isRed() { return suit == Suit.Diamonds || suit == Suit.Hearts || suit == Suit.RedJoker; }
     public boolean isBlack() { return suit == Suit.Clubs || suit == Suit.Spades || suit == Suit.BlackJoker; }
 
     public Rank getRank() { return rank; }
     public Suit getSuit() { return suit; }
-    public int getValue() { return rank.giveValue(); }
+    public int getValue() { return rank.getValue(); }
 
     //public String getID() { return rank+""+suit; }
 
     public String toString() { return rank + " of " + suit; }
-    
-    
+
+
     public ClientCard toClientCard()
     	{
     	ClientCard c=new ClientCard();
--- serverData/PlayingCardUtil.java
+++ serverData/PlayingCardUtil.java
@@ -9,19 +9,29 @@ import serverData.CardStack;
  */
 public class PlayingCardUtil
 	{
-    private static final CardStack<PlayingCard> deck52 = new CardStack<PlayingCard>();
-    private static final CardStack<PlayingCard> jokers = new CardStack<PlayingCard>();
-    static
-        {
-        for(PlayingCard.Suit s : EnumSet.range(PlayingCard.Suit.Clubs, PlayingCard.Suit.Spades))
-            for(PlayingCard.Rank r : EnumSet.range(PlayingCard.Rank.Deuce, PlayingCard.Rank.Ace))
-                deck52.addCard(new PlayingCard(s,r));
+	private static final CardStack<PlayingCard> deck52 = new CardStack<PlayingCard>();
+	private static final CardStack<PlayingCard> jokers = new CardStack<PlayingCard>();
+	static
+		{
+		for(PlayingCard.Suit s : EnumSet.range(PlayingCard.Suit.Clubs, PlayingCard.Suit.Spades))
+			for(PlayingCard.Rank r : EnumSet.range(PlayingCard.Rank.Deuce, PlayingCard.Rank.Ace))
+				deck52.addCard(new PlayingCard(s,r));
 
-        for(PlayingCard.Suit s : EnumSet.range(PlayingCard.Suit.RedJoker, PlayingCard.Suit.BlackJoker))
-            deck52.addCard(new PlayingCard(s,PlayingCard.Rank.Joker));
+			for(PlayingCard.Suit s : EnumSet.range(PlayingCard.Suit.RedJoker, PlayingCard.Suit.BlackJoker))
+				deck52.addCard(new PlayingCard(s,PlayingCard.Rank.Joker));
+		}
 
-        }
+	public static CardStack<PlayingCard> getDeck52() { return deck52; }
+	public static CardStack<PlayingCard> getJokers() { return jokers; }
 
-    public static CardStack<PlayingCard> getDeck52() { return deck52; }
-    public static CardStack<PlayingCard> getJokers() { return jokers; }
+	public static void sortSuit(CardStack<PlayingCard> c)
+		{
+		// TODO
+		}
+
+	public static void sortRank(CardStack<PlayingCard> c)
+		{
+		// TODO
+		}
+
 	}
